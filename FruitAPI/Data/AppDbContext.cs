46d1510dcb2c8c5a27f752a52da1a3cd69ffe921
Temitope Adebiyi <adebiyitope26@gmail.com>
2023-12-01 22:17:50 +0100

    Implemented User Profile Service in FruitAPI
    This commit introduces the UserProfileService in the FruitAPI project. This service is responsible for managing all operations related to user information, leveraging the AppDbContext for database interactions. It includes methods for creating, retrieving, updating, and deleting user profiles, making it a cornerstone for user data management in the Fruit App.
    
    Key Features:
    - Methods for CRUD operations on user profiles.
    - Asynchronous operations ensuring efficient data handling.
    - Integration with Entity Framework Core for database interactions.
    
    This enhancement solidifies the backend's capability to manage user data effectively, a critical aspect of the Fruit App's functionality.

---
+++ FruitAPI/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using FruitAPI.Models;
+
+namespace FruitAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<UserInfo> UserInfo { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
